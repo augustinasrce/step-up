8a5d6896bdea24288f6e7aaf14e495e7d3c70835
Augustinas <augustinas.sueris@gmail.com>
2018-05-03 15:20:54 +0300

    Init

---
+++ src/main/java/com/haka/stepup/StepupApplication.java
@@ -0,0 +1,12 @@
+package com.haka.stepup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StepupApplication.class, args);
+	}
+}
---
+++ src/test/java/com/haka/stepup/StepupApplicationTests.java
@@ -0,0 +1,16 @@
+package com.haka.stepup;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StepupApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
